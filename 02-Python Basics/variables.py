be126858265101b5eb15ffbbc709761048dcc0df
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-13 11:01:33 +0530

    Create variables.py for Python variable practice
    Add a practice file demonstrating Python variable usage, including creation, printing, data types, and multiple assignments.

---
+++ 02-Python Basics/variables.py
@@ -0,0 +1,55 @@
+# Pyhton Variable - Practice file
+
+# 1. What is Variable ?
+# A Variable is container that stores a value in  a memoray.
+
+# 2. Creating Variables
+name = "PRAJAKTA"       #string
+age = "25"              #Integr
+height = 5.7            #float
+is_student = True       #Boolean
+
+# 3. Printing Variables
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# 4. Multiple Assignment
+x = y = z = 20         # All three get value 20
+print(x, y, z)
+
+a, b, c = 1, 2, 3     # Each gets different values
+
+# 5. Data types of Variables
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# 6. changing Variable value
+
+age = 25
+print(age)
+age = 30 
+print(age)        # 30 (value updated)
+
+# 7. String Variables
+first_name = "Prajakta"
+last_name = "Kumar"
+full_name = last_name + " " + last_name
+print(full_name)     #Prajakta Kumar
+
+# 8. Number Variables
+price = 500
+discount = 50 
+final_price = price - discount
+print(final_price)    # 450
+
+# 9. Constat Variables (by convention use UPPERCASE)
+PI = 3.14
+print(PI)
+
+
+
+
