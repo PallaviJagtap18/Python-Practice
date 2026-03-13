8cfc72b97b4199e9c229d7958a9bfb4f063912e7
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-13 20:31:07 +0530

    Create data_types.py

---
+++ 01-Python Basics/data_types.py
@@ -0,0 +1,82 @@
+# Python Data Types
+# 1. Integer (int)
+
+# Definition:
+# An integer is a whole number without a decimal point. It can be positive, negative, or zero.
+
+# Examples:
+# 10, -5, 0, 100
+
+# Python Code Example:
+
+# Integer example
+
+a = 10
+b = -5
+c = 0
+
+print("Value of a:", a)
+print("Value of b:", b)
+print("Value of c:", c)
+print("Type of a:", type(a))
+
+# 2. Float
+
+# Definition:
+# A float is a number that contains a decimal point. It is used to represent real numbers.
+
+# Examples:
+# 10.5, -3.14, 0.0
+
+# Python Code Example:
+
+# Float example
+
+x = 10.5
+y = -3.14
+
+print("Value of x:", x)
+print("Value of y:", y)
+print("Type of x:", type(x))
+
+# 3. String (str)
+
+# Definition:
+# A string is a sequence of characters enclosed in single quotes (' ') or double quotes (" ").
+# Strings are used to store text data.
+
+# Examples:
+# "Python", "Hello World", "123"
+
+# Python Code Example:
+
+# String example
+
+name = "Python"
+message = "Hello World"
+
+print("Name:", name)
+print("Message:", message)
+print("Type of name:", type(name))
+
+# 4. Boolean (bool)
+
+# Definition:
+# A Boolean data type represents only two values:
+
+# True AND  False
+
+# It is mainly used in decision making and conditions.
+
+# Python Code Example:
+
+# Boolean example
+
+is_student = True
+is_logged_in = False
+
+print("Is student:", is_student)
+print("Is logged in:", is_logged_in)
+print("Type:", type(is_student))
+
+
