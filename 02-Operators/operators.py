8f995cec1ce19f28f2011e3065fa6e20e1f10f63
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-14 08:00:30 +0530

    operators

---
+++ 02-Operators/operators.py
@@ -0,0 +1,25 @@
+# Operators in Python
+
+# Operators are special symbols used to perform operations on variables and values.
+# Python provides different types of operators such as arithmetic, comparison, logical, and assignment operators.
+
+# 1. Arithmetic Operators
+
+# Arithmetic operators are used to perform mathematical calculations like addition, subtraction, multiplication, etc.
+
+a = 10
+b = 3
+
+print("Addition: ", a + b )
+print("Subtraction: ", a - b )
+print("Multiplication: ", a * b )
+print("Division: ", a / b )
+print("Modulus: ", a % b )
+print("Power: ", a ** b )
+print("floor division: ", a // b )
+
+# 2. Comparison Operators
+
+# Comparison operators are used to compare two values.
+# They return either True or False.
+
