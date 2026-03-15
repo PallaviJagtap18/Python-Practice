e7e8738edabc35c3fff85ff44e6b00e3e4c7a3de
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-15 08:37:43 +0530

    Update list_slicing.py

--- string/list_slicing.py
+++ string/list_slicing.py
@@ -1,2 +1,9 @@
+# List Slicing
+"""List Slicing (in Python) is the process of extracting a portion of a list by specifying a range of 
+indices using the slicing operator :. It allows you to create a new list containing selected elements from the original list."""
+
 name = "Hello World"
-print(name[0 : 4])
+
+print(name[0 : 4])  # 0 is included and 4 in excluded
+print(name[0 : -5]) # -5 = len(name) - 5
+print(name[3 : 7])
