8a19136da2eabc315341321129f4d39ae1c4dc0b
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-16 20:55:33 +0530

    if_elif_else.py

--- 04-Control flow/if_elif_else.py
+++ 04-Control flow/if_elif_else.py
@@ -4,6 +4,8 @@
 # elif – Short for “else if”; used to check another condition if the previous if condition is False.
 # else – Executes a block of code when none of the above conditions are True.
 
+# Example 1
+
 num = int(input("enter the number:  "))
 
 if(num > 0):
@@ -12,3 +14,14 @@ elif( num == 0 ):
     print("Number is zero")
 else:
     print("Negative number")
+
+# Example 2 
+
+import time 
+hour = int(time.strftime('%H))
+if (hour<12)
+    print("Good Morning")
+elif(hour<17):
+    print("Good Afternoon")
+else:
+    print("Good Evening")
