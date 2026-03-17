ba9bf2972c2b4157732f1725e68e3049c6a71eae
Pallavi Jagtap <pallavijagtap066@gmail.com>
2026-03-17 21:16:11 +0530

    match case

--- 05 -Match Case/match_case.py
+++ 05 -Match Case/match_case.py
@@ -15,3 +15,25 @@ match x:
         print("value of x is 3")
     case _ :
         print("invalid")
+
+
+day = int(input("enter the day(1-7) : ")
+
+match day:
+
+    case 1:
+        print("monday")
+    case 2:
+        print("tuesday")
+    case 3:
+        print("wednesday")
+    case 4:
+        print("thrusday")
+    case 5:
+        print("friday")
+    case 6:
+        print("saturday")
+    case 7:
+        print("sunday")
+    case _:
+        print("invalid day")
