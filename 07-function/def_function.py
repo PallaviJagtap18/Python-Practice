f74c9f614a02cbe32b9e33cc893241875f402f4d
Pallavi Jagtap <pallavijagtap066@gmail.com>
2026-03-21 22:58:46 +0530

    Add sum function to calculate the sum of two numbers

--- 07-function/def_function.py
+++ 07-function/def_function.py
@@ -1,3 +1,8 @@
 # What is function?
 # A function is a reusable block of code that performs a specific task. It helps make programs modular, readable, and easier to debug.
 
+def sum(a,b):
+  return a+b
+
+result = sum(5, 3)
+print("sum ", result)
