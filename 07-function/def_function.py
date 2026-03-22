206c4bfc14070ce6dcdb0104155e6b75207e4f98
Pallavi Jagtap <pallavijagtap066@gmail.com>
2026-03-22 21:55:39 +0530

    Add greet function

--- 07-function/def_function.py
+++ 07-function/def_function.py
@@ -1,8 +1,7 @@
 # What is function?
 # A function is a reusable block of code that performs a specific task. It helps make programs modular, readable, and easier to debug.
 
-def sum(a,b):
-  return a+b
+def greet():
+    print("Hello, World!")
 
-result = sum(5, 3)
-print("sum  = ", result)
+greet()
