fbaca924439df692af03f4132d57b9c5ee84448e
Pallavi Jagtap <pallavijagtap066@gmail.com>
2026-03-22 22:01:34 +0530

    Document function parameters and argument types
    Added explanations and examples for different types of function arguments.

--- 07-function/Parameters_&_ Arguments.py
+++ 07-function/Parameters_&_ Arguments.py
@@ -1,1 +1,29 @@
+# Parameters:
+# Variables listed in the function definition.
 
+#  Arguments:
+# Values passed to the function when calling it.
+
+# Types of Arguments:
+
+# 1.Positional Arguments
+def add(a, b):
+    print(a + b)
+
+add(2, 3)
+
+# 2.Keyword Arguments
+add(a=2, b=3)
+
+# 3.Default Arguments
+def greet(name="Guest"):
+    print("Hello", name)
+
+greet()          # uses default
+greet("Amit")
+
+# 4.Variable-length Arguments
+def total(*numbers):
+    print(sum(numbers))
+
+total(1, 2, 3, 4)
