eff9ba59f7942385b169215a9540302e02d615c5
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-25 17:39:34 +0530

    Create lists.py

---
+++ Data Structure/lists.py
@@ -0,0 +1,22 @@
+#  Lists
+
+# A list is a collection of items that is:
+# Ordered
+# Mutable (changeable)
+# Allows duplicate values
+
+my_list = [1, 2, 3, 4]
+# 🔹 Example
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])   # apple
+
+fruits.append("orange")     # Add item
+
+fruits.remove("banana")     # Remove item
+
+fruits.insert(1, "mango")   # Insert at index
+
+fruits.pop(2)                # Remove item
+
+print(fruits)
