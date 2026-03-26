8ff9bc3930b929607f9f31bf8526842ff7d4e0d8
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-03-26 17:17:17 +0530

    Create tuples.py

---
+++ 08-Data Structure/tuples.py
@@ -0,0 +1,25 @@
+#  Tuples
+# A tuple is a collection of items that is:
+
+# Ordered
+# Immutable (cannot be changed after creation)
+# Allows duplicates
+
+
+my_tuple = (1, 2, 3)
+
+#  Example
+
+numbers = (10, 20, 30)
+print(numbers[1])   
+#  Key Features
+# Faster than lists
+# Cannot modify elements
+# Useful for fixed data
+
+# Important Note
+# numbers[0] = 100      error
+
+#  Tuple Packing & Unpacking
+a, b, c = (1, 2, 3)
+print(a, b, c)
