1e9442a04f85f2dfb7bf9937f12a2742a6a5c27e
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-04-01 20:51:55 +0530

    Create Importing_Modules.py

---
+++ 09 - Modules/Importing_Modules.py
@@ -0,0 +1,18 @@
+# Importing Modules
+
+# You use the import keyword to use a module in your program.
+
+# 1.Basic Import
+
+import math
+print(math.sqrt(16))   
+
+# 2.Import Specific Function
+
+from math import sqrt
+print(sqrt(25))  
+
+# 3.Import with Alias
+
+import math as m
+print(m.pi)  
