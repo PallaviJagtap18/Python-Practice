53794c6ce24e34c6c10fd221e6e6e9c0a46356be
PallaviJagtap18 <pallavijagtap066@gmail.com>
2026-04-19 22:18:53 +0530

    Create Custom_Esceptions.py

---
+++ 11-Exception handling/Custom_Esceptions.py
@@ -0,0 +1,12 @@
+# Custom Exceptions
+# User-defined errors for specific conditions.
+
+class InvalidAgeError(Exception):
+    pass
+
+try:
+    age = int(input("Enter age: "))
+    if age < 18:
+        raise InvalidAgeError("Age must be 18+")
+except InvalidAgeError as e:
+    print(e)
